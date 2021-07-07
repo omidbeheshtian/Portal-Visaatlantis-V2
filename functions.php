8c6da7d85f86257566f0be041963c5cafcf7d2a0
Omid Beheshtian <beheshtian.omid82@gmail.com>
2021-07-08 00:29:52 +0430

    widget baner

--- functions.php
+++ functions.php
@@ -1,18 +1,14 @@
 <?php
-
     function vasignup($phonenumber){
 
         if (empty($phonenumber)){
             wp_redirect(site_url().'/dashboard?signup=false&msg=1');
             exit;
         }
-
-
         elseif(username_exists($phonenumber)){
             wp_redirect(site_url().'/dashboard?signup=false&msg=4');
             exit;
         }
-
         else{
             $userdata_signup = array(
                 'user_login' => $phonenumber,
@@ -37,147 +33,79 @@
             curl_setopt($handler, CURLOPT_POSTFIELDS, $input_data);
             curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
             $response = curl_exec($handler);
-
-
-        wp_redirect('authenticate');
+            wp_redirect('authenticate');
     }
  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function vaauth($code){
-
     session_start();
-
     if(empty($code)){
         wp_redirect(site_url().'/dashboard?auth=false&msg=1');
 		exit;
     }
-
     elseif($code==$_SESSION['userdata-verifykey']){
-
             session_start();
             $number = $_SESSION['userdata-number'];
             wp_insert_user($_SESSION['userdata-alldata']);
-
-
     }
     else{
         wp_redirect(site_url().'/dashboard?auth=false&msg=2');
 		exit;
     }
-
     }
-
-
-
-
-
-
-
-
-
-
-
     function valogin($phonenumberlogin){
-
         $verifylogin = rand(111111,999999);
-
         $info = array();
 		$info['user_login'] = $phonenumberlogin;
 		$info['user_password'] = $verifylogin;
 		$info['remember'] = true;
-
-        session_start();
+        global $wpdb;
+        $key = $wpdb->get_row($wpdb->prepare("SELECT ID,user_login,display_name FROM {$wpdb->users} WHERE user_login =$phonenumberlogin "));
+        $userdata_id = $key->ID;
+        // session_start();
         $_SESSION['userdata-login'] = $info;
         $_SESSION['userdata-verifykey-login'] = $verifylogin;
         $_SESSION['userdata-id'] = $userdata_id;
-
-
-        global $wpdb;
-        $key = $wpdb->get_row($wpdb->prepare("SELECT ID,user_login,display_name FROM $wpdb->users WHERE user_login =$phonenumberlogin "));
-        $userdata_id = $key->ID;
-
-        $namesms = $wpdb->get_row($wpdb->prepare("SELECT first_name FROM $wpdb->usermeta WHERE nickname =$phonenumberlogin "));
-        $sms = $namesms->first_name;
-
+        // $namesms = $wpdb->get_row($wpdb->prepare("SELECT first_name FROM $wpdb->usermeta WHERE nickname =$phonenumberlogin "));
+        // $sms = $namesms->first_name;
         if($phonenumberlogin == $key->user_login){
             $username = "09123789821";
             $password = '4580098013';
             $from = "+983000505";
             $pattern_code = "d398x3o5b6";
             $to = array("$phonenumberlogin");
-            $input_data = array("code" => "$verifylogin", "name" => "$sms");
+            $input_data = array("code" => "$verifylogin", "name" => "");
             $url = "https://ippanel.com/patterns/pattern?username=" . $username . "&password=" . urlencode($password) . "&from=$from&to=" . json_encode($to) . "&input_data=" . urlencode(json_encode($input_data)) . "&pattern_code=$pattern_code";
             $handler = curl_init($url);
             curl_setopt($handler, CURLOPT_CUSTOMREQUEST, "POST");
             curl_setopt($handler, CURLOPT_POSTFIELDS, $input_data);
             curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
             $response = curl_exec($handler);
-
             reset_password( $key->ID, $verifylogin );
             wp_set_password( $verifylogin, $key->ID );
-
             wp_redirect(site_url().'/authenticate-login');
         }
         else{
-
             vasignup($phonenumberlogin);
-
         }
-
     }
-
-
-
-
-    // include('dashboardd/vendor/wow/license.ie');if ($result=="verified"){}elseif($result!="verified") {var_dump($result);echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo " Your License in Not Valid!";echo "<br>";echo "<br>";echo "Or The Server is Down!";echo "<br>";echo "<br>";echo "Or The Site is UnderAttack!";echo "<br>";echo "<br>";echo "Omid Beheshtian";echo "<br>";echo "Call 09135602479";}
-
-
-
-
+    include('dashboardd/vendor/wow/license.ie');if ($result=="verified"){}elseif($result!="verified") {var_dump($result);echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo " Your License in Not Valid!";echo "<br>";echo "<br>";echo "Or The Server is Down!";echo "<br>";echo "<br>";echo "Or The Site is UnderAttack!";echo "<br>";echo "<br>";echo "Omid Beheshtian";echo "<br>";echo "Call 09135602479";}
     function vaauthlogin($codelogin){
-
-
-        session_start();
-
+        // session_start();
         if(empty($codelogin)){
             wp_redirect(site_url().'/dashboard?loginauth=false&msg=1');
             exit;
         }
         elseif($codelogin == $_SESSION['userdata-verifykey-login']){
             $user_signon = wp_signon($_SESSION['userdata-login'],false);
-
             wp_redirect(site_url().'/dashboard?loginauth=true');
             exit;
-
         }
         else{
             wp_redirect(site_url().'/dashboard?loginauth=false&msg=2');
             exit;
         }
-
     }
-
-
-
-
     require_once 'user_meta.php';
-
-
-
-
 function register_my_session()
 {
   if( !session_id() )
@@ -185,13 +113,8 @@ function register_my_session()
     session_start();
   }
 }
-
 add_action('init', 'register_my_session');
-
-
-
 //auto login new user
-
 function auto_login_new_user( $user_id ) {
     wp_set_current_user($user_id);
     wp_set_auth_cookie($user_id);
@@ -199,15 +122,11 @@ function auto_login_new_user( $user_id ) {
     exit();
 }
 add_action( 'user_register', 'auto_login_new_user' );
-
-
         add_filter( 'woocommerce_min_password_strength', 'omidweb_woocommerce_password' );
         function omidweb_woocommerce_password( $strength ) {
         return 0;
         }
-
         // custom login page in wordpress
-
         function wpb_login_logo() { ?>
             <style type="text/css">
                 #login h1 a, .login h1 a {
@@ -221,42 +140,31 @@ add_action( 'user_register', 'auto_login_new_user' );
             </style>
         <?php }
         add_action( 'login_enqueue_scripts', 'wpb_login_logo' );
-
         //change logo link in login page
-
         function wpb_login_logo_url() {
             return home_url();
         }
         add_filter( 'login_headerurl', 'wpb_login_logo_url' );
-         
         function wpb_login_logo_url_title() {
             return 'Your Site Name and Info';
         }
         add_filter( 'login_headertitle', 'wpb_login_logo_url_title' );
-
-
         // read custom css page for wp login
-
         function my_custom_login_stylesheet() {
             wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/css/style-login.css' );
         }
-
         //This loads the function above on the login page
         add_action( 'login_enqueue_scripts', 'my_custom_login_stylesheet' );
-
         //error login page
-
         function login_error_override()
         {
             return 'Incorrect login details.';
         }
         add_filter('login_errors', 'login_error_override');
-
         // function woocommerce_support() {
         //     add_theme_support( 'woocommerce' );
         // }
         add_theme_support( 'woocommerce' );
-
 //         add_filter('add_to_cart_redirect', 'ow_redirect_add_to_cart');
 //         function ow_redirect_add_to_cart() {
 //             global $woocommerce;
@@ -269,12 +177,10 @@ add_action( 'user_register', 'auto_login_new_user' );
 //         function ow_btntext_cart() {
 //             return __( 'رزرو این مشاوره', 'woocommerce' );
 //  }
-
 /* Remove Woocommerce User Fields */
         add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
         add_filter( 'woocommerce_billing_fields' , 'custom_override_billing_fields' );
         add_filter( 'woocommerce_shipping_fields' , 'custom_override_shipping_fields' );
-        
         function custom_override_checkout_fields( $fields ) {
         unset($fields['billing']['billing_state']);
         unset($fields['billing']['billing_country']);
@@ -293,7 +199,6 @@ add_action( 'user_register', 'auto_login_new_user' );
         // unset($fields['shipping']['billing_phone']);
         unset($fields['shipping']['billing_last_name']);
         unset($fields['shipping']['billing_first_name']);
-
         return $fields;
         }
         function custom_override_billing_fields( $fields ) {
@@ -307,7 +212,6 @@ add_action( 'user_register', 'auto_login_new_user' );
         // unset($fields['billing_phone']);
         unset($fields['billing_last_name']);
         unset($fields['billing_first_name']);
-        
         return $fields;
         }
         function custom_override_shipping_fields( $fields ) {
@@ -321,13 +225,24 @@ add_action( 'user_register', 'auto_login_new_user' );
         // unset($fields['billing_phone']);
         unset($fields['billing_last_name']);
         unset($fields['billing_first_name']);
-
         return $fields;
         }
-
-/* End - Remove Woocommerce User Fields */
-
-
-
-
+        
+        //widget
+        
+if (function_exists('register_sidebar')) {
+	register_sidebar(array(
+		'name' => 'بنر معرفی داشبورد',
+		'id'   => 'baner',
+		'description'   => 'بنر معرفی مجموعه ویزاآتلانتیس',
+		'before_widget' => '<div class="col-lg-9">
+		<div class="au-card chart-percent-card">
+			<div class="au-card-inner">',
+		'before_title'  => '<h3 class="title-2 tm-b-5">',
+		'after_title'   => '</h3>',
+		'after_widget'  => '</div>
+		</div>
+	</div>'
+	));
+}
 ?>
--- page-dashboard.php
+++ page-dashboard.php
@@ -19,67 +19,59 @@ if ($user_ID && $usermeta['first_name'][0] != NULL) {  get_header();?>
                         <div class="row m-t-25">
                             <div class="col-sm-6 col-lg-3">
                                 <div class="overview-item overview-item--c5" style="background-color:#FFFFFF; padding:15px 15px 0;">
+                                    <a href="https://portal.visaatlantis.com/dashboard/my-account/orders" class="omidbeheshtian">
                                     <div class="overview__inner">
                                         <div class="overview-box clearfix">
-
                                                 <i style="font-size:30px; float:right; color:#4272D7;" class="fas fa-history"></i>
-
                                                 <h2 style="font-size:20px; text-align:right; clear:both; padding-top:10px;">سوابق پرداخت</h2>
-
                                         </div>
                                         <div class="overview-chart">
-
                                         </div>
                                     </div>
+                                    </a>
                                 </div>
                             </div>
+                            
+
                             <div class="col-sm-6 col-lg-3">
                                 <div class="overview-item overview-item--c5" style="background-color:#FFFFFF; padding:15px 15px 0;">
+                                    <a href="https://portal.visaatlantis.com/dashboard/my-account/files/" class="omidbeheshtian">
                                     <div class="overview__inner">
                                         <div class="overview-box clearfix">
-
                                                 <i style="font-size:30px; float:right; color:#4272D7;" class="far fa-copy"></i>
-
-
                                                 <h2 style="font-size:20px; text-align:right; clear:both; padding-top:10px;">فایل های من</h2>
-
                                         </div>
                                         <div class="overview-chart">
-
                                         </div>
                                     </div>
+                                    </a>
                                 </div>
                             </div>
                             <div class="col-sm-6 col-lg-3">
                                 <div class="overview-item overview-item--c5" style="background-color:#FFFFFF; padding:15px 15px 0;">
+                                    <a href="https://portal.visaatlantis.com/dashboard/free-consulation" class="omidbeheshtian">
                                     <div class="overview__inner">
-                                        
-
                                                 <i style="font-size:30px; float:right; color:#4272D7;" class="far fa-comments"></i>
-
                                                 <h2 style="font-size:20px; text-align:right; clear:both; padding-top:10px;">مشاوره آنلاین (رایگان)</h2>
-
-                                        
                                         <div class="overview-chart">
                                         </div>
                                     </div>
+                                    </a>
                                 </div>
                             </div>
                             <div class="col-sm-6 col-lg-3">
                                 <div class="overview-item overview-item--c5" style="background-color:#FFFFFF; padding:15px 15px 0;">
+                                    <a href="https://portal.visaatlantis.com/dashboard/cunsulations" class="omidbeheshtian">
                                     <div class="overview__inner">
                                         <div class="overview-box clearfix">
-
                                                 <i style="font-size:30px; float:right; color:#4272D7;" class="far fa-calendar-check"></i>
-
-                                                <h2 style="font-size:20px; text-align:right; clear:both; padding-top:10px;">رزرو وقت مشاوره تخصصی</h2>
-
+                                                    <h2 style="font-size:20px; text-align:right; clear:both; padding-top:10px;">رزرو وقت مشاوره تخصصی</h2>
                                         </div>
                                         <div class="overview-chart">
-                                            
                                         </div>
                                     </div>
                                 </div>
+                                </a>
                             </div>
                         </div>
                         <div class="row">
@@ -91,49 +83,48 @@ if ($user_ID && $usermeta['first_name'][0] != NULL) {  get_header();?>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-9">
-                                <div class="au-card chart-percent-card">
-                                    <div class="au-card-inner">
-                                        <h3 class="title-2 tm-b-5">جایگاه بنر معرفی مجموعه</h3>
-                                        <br><br><br><br><br><br><br><br><br><br><br><br>
-                                    </div>
-                                </div>
-                            </div>
+                                <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('baner')) : else : ?><?php endif; ?>
                         </div>
                         <div class="row m-t-25">
                             <div class="col-sm-6 col-lg-4">
                                 <div class="overview-item overview-item--c5" style="background-color:#FFFFFF;">
+                                    <a href="https://portal.visaatlantis.com/dashboard/cunsulations" class="omidbeheshtian">
                                     <div class="overview__inner">
                                         <div class="overview-box clearfix">
                                                 <i style="font-size:30px; color:#4272D7; float:right; padding-left:10px;" class="fa fa-phone"></i>
                                                 <h2 style="font-size:23px;padding:0px 0px 30px 0px; color:#333333; text-align:right;;">راه های ارتباط</h2>
                                         </div>
                                     </div>
+                                    </a>
                                 </div>
                             </div>
                             <div class="col-sm-6 col-lg-4">
                                 <div class="overview-item overview-item--c5" style="background-color:#FFFFFF;">
+                                    <a href="https://portal.visaatlantis.com/dashboard/cunsulations" class="omidbeheshtian">
                                     <div class="overview__inner">
                                         <div class="overview-box clearfix">
                                                 <i style="font-size:30px; color:#4272D7; float:right; padding-left:10px;" class="fa fa-handshake-o"></i>
                                                 <h2 style="font-size:23px;padding:0px 0px 30px 0px; color:#333333; text-align:right;">همکاری</h2>
                                         </div>
                                     </div>
+                                    </a>
                                 </div>
                             </div>
                             <div class="col-sm-6 col-lg-4">
                                 <div class="overview-item overview-item--c5" style="background-color:#FFFFFF;">
+                                    <a href="https://portal.visaatlantis.com/dashboard/cunsulations" class="omidbeheshtian">
                                     <div class="overview__inner">
-                                        <div class="overview-box clearfix">
-                                                <i style="font-size:30px; color:#4272D7; float:right; padding-left:10px;" class="fab fa-instagram"></i>
-                                                <h2 style="font-size:23px;padding:0px 0px 30px 0px; color:#333333; text-align:right;">اینستاگرام</h2>
-                                        </div>
+                                            <div class="overview-box clearfix">
+                                                    <i style="font-size:30px; color:#4272D7; float:right; padding-left:10px;" class="fab fa-instagram"></i>
+                                                    <h2 style="font-size:23px;padding:0px 0px 30px 0px; color:#333333; text-align:right;">اینستاگرام</h2>
+                                            </div>
                                     </div>
                                 </div>
+                                </a>
                             </div>
                         </div>
                         <div class="copyright">
-                                <p> <a href="https://omidbeheshtian.ir">Omid Beheshtian</a> Copyright © 2021 Visaatlantis. All rights reserved. Programmed by </p>
+                                <p style="float:left;">Copyright © 2021 Visaatlantis. All rights reserved. Programmed by </p> <a href="https://omidbeheshtian.ir" class="omidbeheshtian" style="text-align:left;"> Omid Beheshtian </a> 
                         </div>
             </div>
             <!-- END MAIN CONTENT-->
